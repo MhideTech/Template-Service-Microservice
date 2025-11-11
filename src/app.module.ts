05ac0010a7ebf514461a4e30b34bac749c674023
MhideTech <okefolahanolamide2006@gmail.com>
2025-11-12 00:07:07 +0100

    feat: set up postgresql (docker)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TemplatesModule } from './templates/templates.module';
 
 @Module({
-  imports: [TemplatesModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'template_user',
+      password: 'template_password',
+      database: 'template_service_db',
+      autoLoadEntities: true,
+      synchronize: false, // very important
+    }),
+    TemplatesModule,
+  ],
 })
 export class AppModule {}
